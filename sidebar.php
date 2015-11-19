388c536d5141d0abb0e84abf6bb420365bab2275
rollandwalsh <rollandwalsh@gmail.com>
2015-11-19 12:45:00 -0800

    Image size adjustments

--- sidebar.php
+++ sidebar.php
@@ -26,7 +26,7 @@
         <?php while ( $query->have_posts() ) : $query->the_post(); ?>
   			  <div class="tr-sidebar-recent-news-item">
             <?php if ( has_post_thumbnail() ) { ?>
-              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'large' , array( 'class' => '' ) ); ?></a> 
+              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'poster-small' , array( 'class' => '' ) ); ?></a> 
             <?php } ?>
             <div class="tr-sidebar-recent-news-content">
               <header>
