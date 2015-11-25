d49ee750431193c6fe6bef614b425f02760cb526
rollandwalsh <rollandwalsh@gmail.com>
2015-11-25 07:45:23 -0800

    Show page

--- page-show.php
+++ page-show.php
@@ -151,19 +151,17 @@ function isCompanyMember( $name ) {
   </div>
 </section>
 
-<div id="primary" class="tr-page-container">
-	<main id="main" class="show-main" role="main">
+<div id="primary" class="tr-page-container" role="main">
 
   	<?php do_action( 'thirdrail_before_content' ); ?>
   
   	<?php while ( have_posts() ) : the_post(); ?>
-			<h3 class="section-title">About</h3>
-  		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-  			<header>
-  				<h1 class="entry-title"><?php the_title(); ?></h1>
+  		<article class="tr-page-article" id="post-<?php the_ID(); ?>">
+  			<header class="tr-article-header">
+  				<h1 class="tr-page-title show"><?php the_title(); ?></h1>
   			</header>
   			<?php do_action( 'thirdrail_page_before_entry_content' ); ?>
-  			<div class="entry-content">
+  			<div class="tr-show-content">
   				<?php the_content(); ?>
   			</div>
   			<footer>
@@ -174,137 +172,148 @@ function isCompanyMember( $name ) {
   	<?php endwhile;?>
   
   	<?php do_action( 'thirdrail_after_content' ); ?>
+  	
+  	<div class="tr-container">
 		
-		<?php if ( !null == $run_time ) { ?> <p>Runtime: <strong><?php echo $run_time; ?></strong></p> <?php } ?>
-		
-		<ul class="small-block-grid-1 medium-block-grid-2">
-			<?php if ( !null == role('Playwright', $creatives, false) ) { ?>
-			  <li class="playwright">
-			    <h5>Written by</h5>
-			      <?php if ( isCompanyMember( $playwright ) ) { ?>
-    			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button expand"><i class="fa fa-bolt"></i> <?php echo $playwright; ?></a>
-    			  <?php } else { ?>
-    			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other expand"><i class="fa fa-user"></i> <?php echo $playwright; ?></a>
-    			  <?php } ?>
-			  </li>
-      <?php } ?>
-			
-			<?php if ( !null == role('Director', $creatives, false) ) { ?>
-			  <li class="director">
-			    <h5>Directed by</h5>
-			      <?php if ( isCompanyMember( $director ) ) { ?>
-    			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button expand"><i class="fa fa-bolt"></i> <?php echo $director; ?></a>
-    			  <?php } else { ?>
-    			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other expand"><i class="fa fa-user"></i> <?php echo $director; ?></a>
-    			  <?php } ?>
-			  </li>
-      <?php } ?>
-		</ul>
-		
-		<?php if ( !null == $cast ) { ?>
-			<h3 class="section-title">Cast</h3>
-			<div class="show-section">
-			  <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5">
-    			<?php foreach ($cast as $actor) { ?>
-      			<li class="role-block">
-      			  <?php if ( isCompanyMember( $actor[1] ) ) { 
-        			  $actorPage = get_page_by_title( $actor[1] ); ?>
-        			  <?php echo get_the_post_thumbnail( $actorPage->ID, 'portrait', array( 'class' => 'actor-image' ) ); ?>
-        			  <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $actor[1] ) ); ?>/" class="button small expand"><i class="fa fa-bolt"></i> <?php echo $actor[1]; ?></strong></a>
-              <?php } else { 
-                $actorSlug = strtolower( str_replace( ' ', '-', $actor[1] ) ); ?>
-        			  <img src="<?php echo site_url() . '/wp-content/themes/third-rail-wp/assets/img/actors/' . $actorSlug; ?>">
-        			  <a href="<?php echo site_url(); ?>/actor/<?php echo $actorSlug; ?>/" class="button other small expand"><i class="fa fa-user"></i> <?php echo $actor[1]; ?></strong></a>
-              <?php } ?>
-              <p class="actor-role"><span><?php echo $actor[0]; ?></span></p>
-      			</li>
-      		<?php } ?>
-			  </ul>
-			</div>
-		<?php } ?>
-		
-		<?php if ( !null == $creatives ) { ?>
-		  <h3 class="section-title">Creative</h3>
-			<div class="show-section">
-			  <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5">
-    			<?php foreach ($creatives as $creative) { 
-      			if ( !in_array($creative[1], array($director, $playwright) ) ) { ?>
-        			<li class="role-block">
-        			  <?php if ( isCompanyMember( $creative[1] ) ) { ?>
-        			    <h6><?php echo $creative[0]; ?></h6>
-        			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button small expand"><i class="fa fa-bolt"></i> <?php echo $creative[1]; ?></a>
-        			  <?php } else { ?>
-        			    <h6><?php echo $creative[0]; ?></h6>
-        			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other small expand"><i class="fa fa-user"></i> <?php echo $creative[1]; ?></a>
-        			  <?php } ?>
-        			</li>
-      		<?php } } ?>
-        </ul>
-      </div>
-		<?php } ?>
-		
-		<?php
-			$args = array(
-		    'post_type'  	   => 'post',
-		    'posts_per_page' => '5',
-  	    'meta_query' 	   => array( 
-          array(
-            'key'        => 'parent_show',
-            'value'      => get_the_title()
-          )
-  	    ),
-		    'tax_query'      => array(
-  		    array(
-    		    'relation'   => 'OR',
-    		    array(
-			        'taxonomy' => 'post_format',
-			        'field'    => 'slug',
-			        'terms'    => array( 'post-format-gallery' ),
-            ),
-    		    array(
-			        'taxonomy' => 'post_format',
-			        'field'    => 'slug',
-			        'terms'    => array( 'post-format-image' ),
-            ),
-    		    array(
-			        'taxonomy' => 'post_format',
-			        'field'    => 'slug',
-			        'terms'    => array( 'post-format-video' ),
-            ),
-          ),
-        ),
-			);
-		
-			$query = new WP_Query( $args );
-			
-			if ( $query->have_posts() ) { ?>
-  			<h3 class="section-title">Media</h3>
+  		<?php if ( !null == $run_time ) { ?> <p>Runtime: <strong><?php echo $run_time; ?></strong></p> <?php } ?>
+  		
+  		<ul class="small-block-grid-1 medium-block-grid-2">
+  			<?php if ( !null == role('Playwright', $creatives, false) ) { ?>
+  			  <li class="playwright">
+  			    <h5>Written by</h5>
+  			      <?php if ( isCompanyMember( $playwright ) ) { ?>
+      			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button expand"><i class="fa fa-bolt"></i> <?php echo $playwright; ?></a>
+      			  <?php } else { ?>
+      			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other expand"><i class="fa fa-user"></i> <?php echo $playwright; ?></a>
+      			  <?php } ?>
+  			  </li>
+        <?php } ?>
+  			
+  			<?php if ( !null == role('Director', $creatives, false) ) { ?>
+  			  <li class="director">
+  			    <h5>Directed by</h5>
+  			      <?php if ( isCompanyMember( $director ) ) { ?>
+      			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button expand"><i class="fa fa-bolt"></i> <?php echo $director; ?></a>
+      			  <?php } else { ?>
+      			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other expand"><i class="fa fa-user"></i> <?php echo $director; ?></a>
+      			  <?php } ?>
+  			  </li>
+        <?php } ?>
+  		</ul>
+  		
+  		<?php if ( !null == $cast ) { ?>
+  			<h3 class="section-title">Cast</h3>
   			<div class="show-section">
   			  <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5">
-    				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-              <li <?php post_class() ?> id="post-<?php the_ID(); ?>">
-          		  <div class="media-container">
-          		    <?php the_content(); ?>
-          		  </div>
-          		</li>
-    				<?php endwhile; ?>
+      			<?php foreach ($cast as $actor) { ?>
+        			<li class="role-block">
+        			  <?php if ( isCompanyMember( $actor[1] ) ) { 
+          			  $actorPage = get_page_by_title( $actor[1] ); ?>
+          			  <?php echo get_the_post_thumbnail( $actorPage->ID, 'portrait', array( 'class' => 'actor-image' ) ); ?>
+          			  <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $actor[1] ) ); ?>/" class="button small expand"><i class="fa fa-bolt"></i> <?php echo $actor[1]; ?></strong></a>
+                <?php } else { 
+                  $actorSlug = strtolower( str_replace( ' ', '-', $actor[1] ) ); ?>
+          			  <img src="<?php echo site_url() . '/wp-content/themes/third-rail-wp/assets/img/actors/' . $actorSlug; ?>">
+          			  <a href="<?php echo site_url(); ?>/actor/<?php echo $actorSlug; ?>/" class="button other small expand"><i class="fa fa-user"></i> <?php echo $actor[1]; ?></strong></a>
+                <?php } ?>
+                <p class="actor-role"><span><?php echo $actor[0]; ?></span></p>
+        			</li>
+        		<?php } ?>
   			  </ul>
   			</div>
-			<?php }
-		
-      wp_reset_postdata();
-		?>
-		
-		<?php if ( !null == $sponsors ) { ?>
-		  <h3 class="section-title">Sponsors</h3>
-			<div class="show-section">
-			
-			</div>
-		<?php } ?>
+  		<?php } ?>
+  		
+  		<?php if ( !null == $creatives ) { ?>
+  		  <h3 class="section-title">Creative</h3>
+  			<div class="show-section">
+  			  <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5">
+      			<?php foreach ($creatives as $creative) { 
+        			if ( !in_array($creative[1], array($director, $playwright) ) ) { ?>
+          			<li class="role-block">
+          			  <?php if ( isCompanyMember( $creative[1] ) ) { ?>
+          			    <h6><?php echo $creative[0]; ?></h6>
+          			    <a href="<?php echo site_url(); ?>/company/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button small expand"><i class="fa fa-bolt"></i> <?php echo $creative[1]; ?></a>
+          			  <?php } else { ?>
+          			    <h6><?php echo $creative[0]; ?></h6>
+          			    <a href="<?php echo site_url(); ?>/creative/<?php echo strtolower( str_replace( ' ', '-', $creative[1] ) ); ?>/" class="button other small expand"><i class="fa fa-user"></i> <?php echo $creative[1]; ?></a>
+          			  <?php } ?>
+          			</li>
+        		<?php } } ?>
+          </ul>
+        </div>
+  		<?php } ?>
+  		
+  		<?php
+  			$args = array(
+  		    'post_type'  	   => 'post',
+  		    'posts_per_page' => '5',
+    	    'meta_query' 	   => array( 
+            array(
+              'key'        => 'parent_show',
+              'value'      => get_the_title()
+            )
+    	    ),
+  		    'tax_query'      => array(
+    		    array(
+      		    'relation'   => 'OR',
+      		    array(
+  			        'taxonomy' => 'post_format',
+  			        'field'    => 'slug',
+  			        'terms'    => array( 'post-format-gallery' ),
+              ),
+      		    array(
+  			        'taxonomy' => 'post_format',
+  			        'field'    => 'slug',
+  			        'terms'    => array( 'post-format-image' ),
+              ),
+      		    array(
+  			        'taxonomy' => 'post_format',
+  			        'field'    => 'slug',
+  			        'terms'    => array( 'post-format-video' ),
+              ),
+            ),
+          ),
+  			);
+  		
+  			$query = new WP_Query( $args );
+  			
+  			if ( $query->have_posts() ) { ?>
+    			<h3 class="section-title">Media</h3>
+    			<div class="show-section">
+    			  <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5">
+      				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                <li <?php post_class() ?> id="post-<?php the_ID(); ?>">
+            		  <div class="media-container">
+            		    <?php the_content(); ?>
+            		  </div>
+            		</li>
+      				<?php endwhile; ?>
+    			  </ul>
+    			</div>
+  			<?php }
+  		
+        wp_reset_postdata();
+  		?>
+  		
+  		<?php if ( !null == $sponsors ) { ?>
+  		  <h3 class="section-title">Sponsors</h3>
+  			<div class="show-section">
+  			
+  			</div>
+  		<?php } ?>
 
-	</main><!-- #main -->
+	</div>
 	
 </div><!-- #primary -->
+
+<section class="tr-home-calendar">
+  <div class="tr-container">
+    <div id="trCalendar"></div>
+    <div class="tr-calendar-display" id="trCalendarDisplay">
+      <div class="tr-calendar-loading">Loading <i class="fa fa-spinner fa-spin"></i></div>
+    </div>
+  </div>
+</section>
 
 <?php get_sidebar( 'show' ); ?>
 
@@ -312,6 +321,6 @@ function isCompanyMember( $name ) {
 
 <script>
   $(function() {
-    return getEvents(api, createMonths, show);
+    return getEvents(api, createMonths);
   });
 </script>
