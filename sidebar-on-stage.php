12dd5397c28b68e88a4b8e6e7b195d3c94c18b32
Rolland Walsh <rollandwalsh@gmail.com>
2015-12-12 15:26:40 -0800

    sidebar class prefix fix

--- sidebar-on-stage.php
+++ sidebar-on-stage.php
@@ -8,52 +8,8 @@
  */
 
 ?>
-<aside id="sidebar" class="tr-page-sidebar">
+<aside id="sidebar" class="rn-page-sidebar">
 	<?php do_action( 'thirdrail_before_sidebar' ); ?>
-	
-	<article class="tr-sidebar-widget search">
-    <h3 class="tr-sidebar-section-title">Search</h3>
-	  <?php do_action( 'thirdrail_before_searchform' ); ?>
-    <form class="tr-search-form" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    	<?php do_action( 'thirdrail_searchform_top' ); ?>
-    	<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'thirdrail' ); ?>">
-    	<?php do_action( 'thirdrail_searchform_before_search_button' ); ?>
-      <button type="submit" id="searchsubmit"><i class="fa fa-search fa-lg"></i></button>
-    	<?php do_action( 'thirdrail_searchform_after_search_button' ); ?>
-    </form>
-    <?php do_action( 'thirdrail_after_searchform' ); ?>
-	</article>
-  		
-	<?php
-		$args = array(
-	    'post_type'  	   => 'post',
-	    'post_status'    => 'publish',
-	    'posts_per_page' => 1,
-	    'category_name'  => 'this-week'
-		);
-	
-		$query = new WP_Query( $args );
-		
-		if ( $query->have_posts() ) { ?>
-  		<article class="tr-sidebar-widget post">
-  			<h3 class="tr-sidebar-section-title">This Week at Third Rail</h3>
-  			<div class="tr-sidebar-post this-week">
-  				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-  					<header>
-  						<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-  					</header>
-  					<div class="tr-sidebar-post-content">
-  						<?php the_excerpt(); ?>
-  					</div>
-  					<footer>
-  					</footer>
-  				<?php endwhile; ?>
-  			</div>
-  		</article>
-		<?php }
-	
-    wp_reset_postdata();
-	?>
-	
+	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
 	<?php do_action( 'thirdrail_after_sidebar' ); ?>
 </aside>
